6b9355bdf2c207dbd06b4ae0227a1c035f225c13
Brendan Toolan <btoolan73@gmail.com>
2019-08-31 00:11:23 +0100

    First

---
+++ MobileAppDevRepeatProject/MobileAppDevRepeatProject/Views/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using MobileAppDevRepeatProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileAppDevRepeatProject.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MasterBehavior = MasterBehavior.Popover;
+
+            MenuPages1.Add((int)MenuItemType.Browse, (NavigationPage)Detail);
+        }
+
+        public Dictionary<int, NavigationPage> MenuPages1 { get; set; } = new Dictionary<int, NavigationPage>();
+
+        public async Task NavigateFromMenu(int id)
+        {
+            if (!MenuPages1.ContainsKey(id))
+            {
+                switch (id)
+                {
+                    case (int)MenuItemType.Browse:
+                        MenuPages1.Add(id, new NavigationPage(new ItemsPage()));
+                        break;
+                    case (int)MenuItemType.About:
+                        MenuPages1.Add(id, new NavigationPage(new AboutPage()));
+                        break;
+                }
+            }
+
+            var newPage = MenuPages1[id];
+
+            if (newPage != null && Detail != newPage)
+            {
+                Detail = newPage;
+
+                if (Device.RuntimePlatform == Device.Android)
+                    await Task.Delay(100);
+
+                IsPresented = false;
+            }
+        }
+    }
+}
