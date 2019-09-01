e1183b710d6c84825616d5d57642a1879717c93d
Brendan Toolan <btoolan73@gmail.com>
2019-09-01 01:48:45 +0100

    Fifth Commit

--- MobileAppDevRepeatProject/MobileAppDevRepeatProject/ViewModels/ContactModel.cs
+++ MobileAppDevRepeatProject/MobileAppDevRepeatProject/ViewModels/ContactModel.cs
@@ -1,17 +1,39 @@
-﻿using System;
+﻿using MobileAppDevRepeatProject.Models;
+using MobileAppDevRepeatProject.Views;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace MobileAppDevRepeatProject.ViewModels
 {
-    class ContactModel : BaseViewModel
+    public class ContactModel : BaseViewModel
     {
+
+        public ObservableCollection<Item> Items { get; set; }
+        public Command LoadItemsCommand { get; set; }
+
         public ContactModel()
         {
-            Title = "Course";
             
+            Title = "Course";
+            Items = new ObservableCollection<Item>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+            MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
+            {
+                var newItem = item as Item;
+                Items.Add(newItem);
+                await DataStore.AddItemAsync(newItem);
+            });
+
         }
 
+        private Task<Func<object, bool>> ExecuteLoadItemsCommand()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
